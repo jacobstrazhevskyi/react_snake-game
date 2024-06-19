e9ca2fc757123c54e579654e8fee25845875bd59
jacobstrazhevskyi <jacob.strazhevskyi@gmail.com>
2024-06-19 20:48:31 +0300

    implemented Board in Container

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import {
+  Container,
+  styled,
+} from '@mui/material';
+
+import { Board } from '../Board';
+
+import '../../reset.css';
+
+const StyledContainer = styled(Container)({
+  height: '100vh',
+  overflow: 'hidden',
+});
+
 const App: React.FC = () => (
-  123
+  <StyledContainer>
+    <Board />
+  </StyledContainer>
 );
 
 export default App;
