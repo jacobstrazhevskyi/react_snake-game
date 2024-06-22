40f9e532c27b6cf99c6bd196d3316bbbd4a48bea
jacobstrazhevskyi <jacob.strazhevskyi@gmail.com>
2024-06-22 21:34:13 +0300

    implemented board height adaptability

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import uuid from 'react-uuid';
 
@@ -12,44 +12,38 @@ type StyledBoxCustomProps = {
   cellFullness: 'food' | 'snake' | 0,
 };
 
+type StyledBoardBoxCustomProps = {
+  cellsSize: number | undefined,
+}
+
 const BOARD_SIZE = 15;
 
-const StyledBoardBox = styled(Box)({
+const StyledBox = styled(Box)({
   width: '100%',
   height: '100%',
-  maxWidth: '500px',
   display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
   justifyContent: 'center',
-  margin: 'auto',
+  alignItems: 'center',
 });
 
-const StyledBoardRowBox = styled(Box)({
+const StyledBoardBox = styled(Box, {
+  shouldForwardProp: (prop) => prop !== 'cellsSize',
+})<StyledBoardBoxCustomProps>(({ cellsSize }) => ({
+  display: 'grid',
+  gridTemplateColumns: `repeat(${BOARD_SIZE}, ${cellsSize}px)`,
+  gridTemplateRows: `repeat(${BOARD_SIZE}, ${cellsSize}px)`,
+}));
+
+const StyledRowBox = styled(Box)({
+  display: 'contents',
+});
+
+const StyledColumnBox = styled(Box)({
   display: 'flex',
-  width: '100%',
-});
-
-const StyledBoardCellBox = styled(
-  Box,
-  { shouldForwardProp: (prop) => prop !== 'cellFullness' },
-)<StyledBoxCustomProps>(({ cellFullness }) => {
-  let cellColor;
-
-  if (cellFullness === 'snake') {
-    cellColor = 'green';
-  } else if (cellFullness === 'food') {
-    cellColor = 'red';
-  } else {
-    cellColor = '';
-  }
-
-  return {
-    width: '100%',
-    aspectRatio: '1',
-    outline: '1px solid rgb(134, 154, 189)',
-    backgroundColor: cellColor,
-  };
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: '#4caf50',
+  color: 'white',
 });
 
 export const Board: React.FC = () => {
@@ -57,18 +51,64 @@ export const Board: React.FC = () => {
     new Array(BOARD_SIZE).fill(0).map(_row => new Array(BOARD_SIZE).fill(0)),
   );
 
+  const [firstRender, setFirstRender] = useState(true);
+
+  const [finalCellSize, setFinalCellSize] = useState<number>();
+
+  let prevWindowSize: number;
+
+  const handleResize = () => {
+    const currentWindowSize = window.innerHeight + window.innerWidth;
+
+    if (prevWindowSize === currentWindowSize) {
+      return;
+    }
+
+    const cellSize = Math.min(window.innerWidth / BOARD_SIZE, window.innerHeight / BOARD_SIZE);
+
+    const maxCellSize = 500 / BOARD_SIZE;
+
+    if (cellSize > maxCellSize) {
+      setFinalCellSize(maxCellSize);
+    } else {
+      setFinalCellSize(cellSize);
+    }
+
+    // We add them because it doesnt matter which value we have in each of this
+    prevWindowSize = window.innerHeight + window.innerHeight; 
+  };
+
+  useEffect(() => {
+    if (firstRender) {
+      handleResize();
+      setFirstRender(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [board]);
+
   return (
-    <StyledBoardBox>
-      {board.map(row => (
-        <StyledBoardRowBox key={uuid()}>
-          {row.map(cell => (
-            <StyledBoardCellBox
-              key={uuid()}
-              cellFullness={cell}
-            />
-          ))}
-        </StyledBoardRowBox>
-      ))}
-    </StyledBoardBox>
+    <StyledBox>
+      <StyledBoardBox
+        cellsSize={finalCellSize}
+      >
+        {board.map(row => (
+          <StyledRowBox key={uuid()}>
+            {row.map(_cell => (
+              <StyledColumnBox
+                key={uuid()}
+              // cellFullness={cell}
+              />
+            ))}
+          </StyledRowBox>
+        ))}
+      </StyledBoardBox>
+    </StyledBox>
   );
 };
